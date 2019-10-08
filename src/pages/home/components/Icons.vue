ad31992e9d4a882faca5a2add0c01ca92b8c5e34
Xuedixuedi <958190137@qq.com>
2019-10-09 00:07:20 +0800

    图标写了一半

---
+++ src/pages/home/components/Icons.vue
@@ -0,0 +1,108 @@
+<template>
+  <div class="icons">
+    <swiper>
+      <swiper-slide>
+        <div class="icon">
+          <div class="icon-img">
+            <img
+              class="icon-img-content"
+              src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png"
+            />
+          </div>
+          <p class="icon-desc">热门景点</p>
+        </div>
+      </swiper-slide>
+      <swiper-slide>
+        <div class="icon">
+          <div class="icon-img">
+            <img
+              class="icon-img-content"
+              src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png"
+            />
+          </div>
+          <p class="icon-desc">热门景点</p>
+        </div>
+      </swiper-slide>
+    </swiper>
+  </div>
+</template>
+<script>
+export default {
+  name: "HomeIcons",
+  data() {
+    return {
+      iconList: [
+        {
+          id: "0001",
+          imgUrl:
+            "http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png",
+          desc: "景点门票"
+        },
+        {
+          id: "0002",
+          imgUrl:
+            "http://img1.qunarzz.com/piao/fusion/1803/76/eb88861d78fb9902.png",
+          desc: "动植物园"
+        },
+        {
+          d: "0001",
+          imgUrl:
+            "http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png",
+          desc: "景点门票"
+        },
+        {
+          d: "0001",
+          imgUrl:
+            "http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png",
+          desc: "景点门票"
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/varibles.styl';
+
+.icons >>> .swiper-container {
+  height: 0;
+  padding-bottom: 50%;
+}
+
+.icon {
+  position: relative;
+  overflow: hidden;
+  float: left;
+  width: 25%;
+  height: 0;
+  padding-bottom: 25%;
+
+  .icon-img {
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0.44rem;
+    box-sizing: border-box;
+
+    .icon-img-content {
+      display: block;
+      margin: 0 auto;
+      height: 100%;
+    }
+  }
+
+  .icon-desc {
+    display: block;
+    position: absolute;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    height: 0.44rem;
+    line-height: 0.44rem;
+    text-align: center;
+    color: $darkTextColor;
+  }
+}
+</style>

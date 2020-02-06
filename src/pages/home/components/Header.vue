a2c15e691d00456c3a652556f3d2ed7b2aba9f3e
Xuedixuedi <958190137@qq.com>
2020-02-06 15:53:47 +0800

    city router finish

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -6,10 +6,12 @@
         <div class="header-input">
             <span class="iconfont">&#xe632;</span>输入城市/景点/游玩主题
         </div>
-        <div class="header-right">
-            {{ this.city }}
-            <span class="iconfont arrow-icon">&#xe64a;</span>
-        </div>
+        <router-link to="/city">
+            <div class="header-right">
+                {{ this.city }}
+                <span class="iconfont arrow-icon">&#xe64a;</span>
+            </div>
+        </router-link>
     </div>
 </template>
 
@@ -27,7 +29,7 @@ export default {
 
 .header {
   display: flex;
-  line-height: 0.86rem;
+  line-height: $headerHeight;
   background: $bgColor;
   color: #ffffff;
 
@@ -54,6 +56,7 @@ export default {
   }
 
   .header-right {
+    color: #ffffff;
     width: 1.24rem;
     text-align: center;
 

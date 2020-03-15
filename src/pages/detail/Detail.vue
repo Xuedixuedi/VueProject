47da2be068d403ac46a8153500c40f84664b5a3c
lxd <xuedixuedi@lxdbook.local>
2020-03-15 19:51:35 +0800

    finish detail-list

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -2,6 +2,7 @@
     <div>
         <detail-banner></detail-banner>
         <detail-header></detail-header>
+        <detail-list :list="list"></detail-list>
         <div class="content"></div>
     </div>
 </template>
@@ -9,11 +10,47 @@
 <script>
 import DetailBanner from "./components/Banner"
 import DetailHeader from "./components/Header"
+import DetailList from "./components/List"
 export default {
     name: "Detail",
     components: {
         DetailBanner,
-        DetailHeader
+        DetailHeader,
+        DetailList
+    },
+    data() {
+        return {
+            list: [
+                {
+                    title: "成人票",
+                    children: [
+                        {
+                            title: "成人套票"
+                        },
+                        {
+                            title: "成人单日票",
+                            children: [
+                                {
+                                    title: "单日全票"
+                                },
+                                {
+                                    title: "单日缆车票"
+                                }
+                            ]
+                        }
+                    ]
+                },
+                {
+                    title: "学生票"
+                },
+                {
+                    title: "儿童票"
+                },
+                {
+                    title: "特惠票"
+                }
+            ]
+        }
     }
 }
 </script>
